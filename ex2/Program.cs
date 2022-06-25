3e3d638cf6bd55ad5743a1df10c42fa5f4d12bcb
oleg <legik7127@gmail.com>
2022-06-25 22:36:55 +0300

    Runing ex2

---
+++ ex2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 2
+Console.WriteLine("привет - введите 2 числа");
+double number1 = 3.3;
+double number2 = 3.1;
+if(number1 > number2)
+{ 
+    Console.WriteLine (number1);
+   Console.WriteLine ("больше");
+}
+else
+{
+    Console.WriteLine (number2);
+Console.WriteLine ("больше");
+}
