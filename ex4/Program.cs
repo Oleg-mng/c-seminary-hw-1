a3dd681d3c0725cfb5fccccbdee4a6303f383503
oleg <legik7127@gmail.com>
2022-06-25 22:40:14 +0300

    Runing ex4

---
+++ ex4/Program.cs
@@ -0,0 +1,9 @@
+﻿//Задача 4, Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+int number1 = 186;
+int number2 = 19;
+int number3 = -10;
+int max = number1;
+if (number1 > max) max = number1;
+if (number2 > max) max = number2;
+if (number3 > max) max = number3;
+Console.WriteLine(max);
