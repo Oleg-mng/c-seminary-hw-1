249fcb55de46658eb2af172209863afd954eb96b
oleg <legik7127@gmail.com>
2022-06-26 08:04:35 +0300

    Runing ex8

---
+++ ex8/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Определение числа на четность");
+int numberx = 12;
+int i = 0;
+Console.WriteLine(numberx);
+if (numberx % 2 == 0) 
+{
+    Console.WriteLine ("Число нечетное");
+}
+else 
+{
+    Console.WriteLine ("Число четное");
+}
