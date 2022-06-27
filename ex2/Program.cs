71b2e5252ae35e63bf526bf3afa5130ae0734ce9
oleg <legik7127@gmail.com>
2022-06-27 08:06:30 +0300

    Дорешали задачу 8

--- ex2/Program.cs
+++ ex2/Program.cs
@@ -1,14 +1,13 @@
-﻿// Задача 2
-Console.WriteLine("привет - введите 2 числа");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("привет - даны 2 числа");
 double number1 = 3.3;
 double number2 = 3.1;
+Console.WriteLine (number1 +" и "+ number2);
 if(number1 > number2)
 { 
-    Console.WriteLine (number1);
-   Console.WriteLine ("больше");
+    Console.WriteLine (number1 +" больше");
 }
 else
 {
-    Console.WriteLine (number2);
-Console.WriteLine ("больше");
+    Console.WriteLine (number2 + " больше");
 }
--- ex4/Program.cs
+++ ex4/Program.cs
@@ -2,8 +2,9 @@
 int number1 = 186;
 int number2 = 19;
 int number3 = -10;
+Console.WriteLine($"Даны числа: {number1}, {number2}, {number3}");
 int max = number1;
 if (number1 > max) max = number1;
 if (number2 > max) max = number2;
 if (number3 > max) max = number3;
-Console.WriteLine(max);
+Console.WriteLine($"{max} - это максимальное число");
